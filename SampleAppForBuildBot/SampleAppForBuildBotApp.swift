c8d84157cbd3d9b2393eb65634952fefcd33bc24
Anfernee Viduya <development@anfernee.me>
2024-06-01 11:30:45 -0700

    Initial Commit

---
+++ SampleAppForBuildBot/SampleAppForBuildBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppForBuildBotApp.swift
+//  SampleAppForBuildBot
+//
+//  Created by Anfernee on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppForBuildBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
